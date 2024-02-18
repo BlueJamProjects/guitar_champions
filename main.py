21d5c1d93761e3b757443bd1be69ecc9cd8a59b0
jonas <jonasaustinland@tamu.edu>
2024-02-18 14:20:52 -0600

    Simple level select

--- main.py
+++ main.py
@@ -20,169 +20,41 @@ from pygame.locals import (
     QUIT,
 )
 
-# Define constants for the screen width and height
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+
+# Import levels
+import level1
+import level2
 
 
-# Define the Player object extending pygame.sprite.Sprite
-# Instead of a surface, we use an image for a better looking sprite
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.image.load("assets/images/characters/protaganist.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect()
-        
-        #sets the top at an offset of 340 to get him to look like he's standing on the ground
-        self.rect.top = 340
 
-    # Move the sprite based on keypresses
-    def update(self, pressed_keys):
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+current_level = 2
 
-        # Keep player on the screen
-        if self.rect.left < 0:
-            self.rect.left = 0
-        elif self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        elif self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+def select_level(name, index):
+        # Do the job here !
+    current_level = 2
+    print(current_level)
 
 
-class Note(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Note, self).__init__()
+def start_level():
+    print(current_level)
+    match current_level:
+        case 1:
+            level1.start()
 
-        self.surf = pygame.image.load("assets/images/notes/C.png").convert()
+        case 2:
+            level2.start()
 
-        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-        # The starting position is randomly generated
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT-200),
-            )
-        )
+        case _:
+            level1.start()
 
-    # Move the Note based on a constant speed
-    # Remove it when it passes the left edge of the screen
-    def update(self):
-        self.rect.move_ip(-5, 0)
-        if self.rect.right < 0:
-            self.kill()
-
-
-# Setup for sounds, defaults are good
-pygame.mixer.init()
-
-# Initialize pygame
 pygame.init()
-
-    # Setup the clock for a decent framerate
-clock = pygame.time.Clock()
-
-
-
-def set_difficulty(value, difficulty):
-    # Do the job here !
-    pass
-
-def first_level():
-
-    # Create the screen object
-    # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    bg_img = pygame.image.load('assets/images/backgrounds/mario level.png')
-    bg_img = pygame.transform.scale(bg_img,(SCREEN_WIDTH,SCREEN_HEIGHT))
-
-    # Create custom events for adding a Note
-    ADDNote = pygame.USEREVENT + 2
-    pygame.time.set_timer(ADDNote, 2500)
-
-    # Create our 'player'
-    player = Player()
-
-    # Create groups to hold Note sprites, and all sprites
-    # - Notes is used for position updates
-    # - all_sprites isused for rendering
-    Notes = pygame.sprite.Group()
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(player)
-
-    # Load and play our background music
-    pygame.mixer.music.load("assets/sounds/background-music/smoke-on-water.mp3")
-    pygame.mixer.music.play(loops=-1)
-
-
-    
-
-
-    # Variable to keep our main loop running
-    running = True
-
-    # Our main loop
-    while running:
-        # Look at every event in the queue
-        for event in pygame.event.get():
-            # Did the user hit a key?
-            if event.type == KEYDOWN:
-                # Was it the Escape key? If so, stop the loop
-                if event.key == K_ESCAPE:
-                    running = False
-
-            # Did the user click the window close button? If so, stop the loop
-            elif event.type == QUIT:
-                running = False
-
-            # Should we add a new Note?
-            elif event.type == ADDNote:
-                # Create the new Note, and add it to our sprite groups
-                new_Note = Note()
-                Notes.add(new_Note)
-                all_sprites.add(new_Note)
-
-        # Get the set of keys pressed and check for user input
-        pressed_keys = pygame.key.get_pressed()
-        player.update(pressed_keys)
-
-        # Update the position of our enemies and Notes
-        Notes.update()
-
-        # Fill the screen with the background image
-        screen.blit(bg_img,(0,0))
-
-
-        # Draw all our sprites
-        for entity in all_sprites:
-            screen.blit(entity.surf, entity.rect)
-
-
-        # Flip everything to the display
-        pygame.display.flip()
-
-        # Ensure we maintain a 30 frames per second rate
-        clock.tick(30)
-
-    # At this point, we're done, so we can stop and quit the mixer
-    pygame.mixer.music.stop()
-    pygame.mixer.quit()
-
-
-
 
 surface = pygame.display.set_mode((600, 400))
 
-menu = pygame_menu.Menu('Welcome', 400, 300,theme=pygame_menu.themes.THEME_BLUE)
+menu = pygame_menu.Menu('Guitar Champions', 400, 300,theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.text_input('Name :', default='John Doe')
-menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add.button('Play', first_level)
+menu.add.selector('Level Select:', [('One', 1), ('Two', 2)], onchange=select_level)
+menu.add.button('Play', start_level)
+menu.add.button('Tutorial', level1.start)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(surface)
