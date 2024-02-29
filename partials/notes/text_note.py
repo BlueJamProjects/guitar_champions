33a5ed31f6e5293f52b55c40f59ec0835851e33a
Jonas Land <bluejamprojects@gmail.com>
2024-02-29 17:04:14 -0600

    Removed played correctly

--- partials/notes/text_note.py
+++ partials/notes/text_note.py
@@ -22,7 +22,6 @@ class Note(pygame.sprite.Sprite):
         # Initialize Pygame
         pygame.init()
 
-        self.played_correctly = False
 
         # Set up font 
         self.font = pygame.font.SysFont(font_name, font_size)
